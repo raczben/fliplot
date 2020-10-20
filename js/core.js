50a9833e4466e1b0eafacc75ad1a22ddbc4ef4d9
Benedek Racz <betontalpfa@gmail.com>
2020-10-20 17:22:58 +0200

    cleanup core

--- js/core.js
+++ js/core.js
@@ -23,6 +23,25 @@ export var waveformDB = {};
 export var now = -1; // Todo should be integrated to signalDB
 
 /**
+ * Value change type builds up the wave list of the signal. Each element describes a value change of
+ * a signal.
+ * 
+ * @typedef {Object} valueChange_t 
+ * @property {number} time Simulation time in the unit of the simulation timePrecision.
+ * @property {string} val The raw value in binary. Each character represents a bit in the bitvector.
+ * @property {string} [hex] A derived value from raw val. Optional: calculated only when the user
+ *      wants to see hex values. Each hex digit will be X and Z if there is an X or Z bit value in
+ *      its region.
+ * @property {number} [s30] Fixed point float number. First character (s/u) note signed and unsigned
+ *      format. The number of bits used to represent the integer value (the number of bits above the
+ *      decimal point) Note, that the full word length is defined at signal level. So the len and
+ *      this numbers are equal, the value is a fixed point integer. Derived, optional as above.
+ *      Note, that if any X or Z located in the raw binary format, this value will be NaN.
+ * @property {number} [float] Single point float number. Derived, optional as above.
+ * @property {number} [double] Double point float number. Derived, optional as above.
+*/
+
+/**
  * Signal type-definition.
  * @typedef {Object} signal_t 
  * @property {string[]} hierarcy
@@ -30,7 +49,7 @@ export var now = -1; // Todo should be integrated to signalDB
  * @property {string[]} references
  * @property {string} type
  * @property {string} vcdid
- * @property {Object[]} wave
+ * @property {valueChange_t[]} wave
  * @property {number} width
 */
 
@@ -56,6 +75,8 @@ export var now = -1; // Todo should be integrated to signalDB
  * @typedef {Object} simDB_t
  * @property {signal_t[]} signals
  * @property {number} now
+ * @property {number} timePrecision
+ * @property {number} timeUnit
 */
 
 export function setSimDB(db, n){
@@ -65,6 +86,12 @@ export function setSimDB(db, n){
     addAllWaveSignal();
 }
 
+/**
+ * Insert a new signal to waveform window.
+ * 
+ * @param {*} signalID 
+ * @param {number} position 
+ */
 export function insertWaveSignal(signalID, position=-1){
     /** @type {waveformRow_t} rowItem */
     const rowItem = {
@@ -81,44 +108,13 @@ export function insertWaveSignal(signalID, position=-1){
     waveformDB.rows.splice(position, 0, rowItem);
 }
 
+/**
+ * Add all signal from the simDB to the waveform window.
+ */
 export function addAllWaveSignal(){
     simDB.signals.forEach((_element, index) => {
         insertWaveSignal(index);
     });
-}
-
-
-/**
- * 
- * @param {Object} vcdArr 
- * @param {int} i 
- */
-export function getTimeAtI(vcdArr, i) {
-    if (i < 0){
-        throw 'Negative index';
-    }
-    if (i < vcdArr.length){
-        return vcdArr[i].time;
-    }
-    if (i == vcdArr.length){
-        return now;
-    }
-    else {
-        throw 'Index is too great';
-    }
-}
-
-export function getValueAtI(vcdArr, i, def) {
-    if (i < 0){
-        if(def !== undefined){
-            return def;
-        }
-        throw 'Negative index';
-    }
-    if (i >= vcdArr.length){
-        i = vcdArr.length -1;
-    }
-    return vcdArr[i].val;
 }
 
 export function updateDBNow(){
@@ -140,10 +136,127 @@ export function updateDBInitialX(){
         }
     });
 }
+  
+/******************************************************************************
+ * 
+ * Get time and value
+ * 
+ ******************************************************************************/
+
+/**
+ * 
+ * @param {signal_t} signal 
+ * @param {number} time 
+ */
+export function getChangeIndexAt(signal, time) {
+    var idx = binarySearch(signal.wave, time, (time, wave) => {
+      return time - wave.time;
+    })
+    if (idx < 0) {
+      idx = -idx - 2;
+    }
+    return idx;
+}
+
+/**
+ * 
+ * @param {valueChange_t[]} vcdArr 
+ * @param {int} i 
+ */
+export function getTimeAtI(vcdArr, i) {
+    if (i < 0){
+        throw 'Negative index';
+    }
+    if (i < vcdArr.length){
+        return vcdArr[i].time;
+    }
+    if (i == vcdArr.length){
+        return now;
+    }
+    else {
+        throw 'Index is too great';
+    }
+}
+
+/**
+ * 
+ * @param {valueChange_t[]} vcdArr 
+ * @param {number} i 
+ * @param {number} def 
+ */
+export function getValueAtI(vcdArr, i, def) {
+    if (i < 0){
+        if(def !== undefined){
+            return def;
+        }
+        throw 'Negative index';
+    }
+    if (i >= vcdArr.length){
+        i = vcdArr.length -1;
+    }
+    return vcdArr[i].val;
+}
+
+/**
+ * 
+ * @param {signal_t} signal 
+ * @param {number} time 
+ * @param {number} def 
+ */
+export function getValueAt(signal, time, def='- NA -') {
+    try {
+        const idx = getChangeIndexAt(signal, time);
+        const wave = signal.wave[idx];
+        return wave.val;
+      }
+      catch (err) {
+        return def;
+      }
+}
+  
+
+/******************************************************************************
+ * 
+ * Interface API
+ * 
+ ******************************************************************************/
+
+  
+/**
+ * This function returns simulation time of the previous/next (arbirtary) transitition of the given
+ * signal.
+ * 
+ * @param {signal_t} signal 
+ * @param {number} time 
+ * @param {number} deltaTransition 
+ */
+export function getTimeAnyTransition(signal, time, deltaTransition) {
+    const idx = getChangeIndexAt(signal, time);
+    if(deltaTransition < 0){
+        // previous nth change
+        const changeT = getTimeAtI(signal.wave, idx);
+        if(changeT != time){
+            // cursor is not located at value change
+            deltaTransition++;
+        }
+    }
+    const t = getTimeAtI(signal.wave, idx+deltaTransition);
+    console.log(t)
+    return t;
+}
+
+
+/******************************************************************************
+ * 
+ * OTHER / UTIL FUNCTIONS
+ * 
+ ******************************************************************************/
+
 
 /*
  * Binary search in JavaScript.
- * Returns the index of of the element in a sorted array or (-n-1) where n is the insertion point for the new element.
+ * Returns the index of of the element in a sorted array or (-n-1) where n is the insertion point
+ * for the new element.
  * Parameters:
  *     ar - A sorted array
  *     el - An element to search for
@@ -172,51 +285,3 @@ function binarySearch(ar, el, compare_fn) {
     }
     return -m - 1;
 }
-  
-  
-export function getChangeIndexAt(signal, time) {
-    var idx = binarySearch(signal.wave, time, (time, wave) => {
-      return time - wave.time;
-    })
-    if (idx < 0) {
-      idx = -idx - 2;
-    }
-    return idx;
-}
-  
-  
-export function getValueAt(signal, time, def='- NA -') {
-    try {
-        const idx = getChangeIndexAt(signal, time);
-        const wave = signal.wave[idx];
-        return wave.val;
-      }
-      catch (err) {
-        return def;
-      }
-}
-  
-  
-/**
- * This function returns simulation time of the previous/next (arbirtary) transitition of the given
- * signal.
- * 
- * @param {Object} signal 
- * @param {int} time 
- * @param {int} deltaTransition 
- */
-export function getTimeAnyTransition(signal, time, deltaTransition) {
-    const idx = getChangeIndexAt(signal, time);
-    if(deltaTransition < 0){
-        // previous nth change
-        const changeT = getTimeAtI(signal.wave, idx);
-        if(changeT != time){
-            // cursor is not located at value change
-            deltaTransition++;
-        }
-    }
-    const t = getTimeAtI(signal.wave, idx+deltaTransition);
-    console.log(t)
-    return t;
-}
-  
